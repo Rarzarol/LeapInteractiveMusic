5aba1f8b4a1e328dcd1662cdc59aa055fe75011d
Roman Arzaroli <roman.arzaroli@gmail.com>
2014-01-22 12:16:43 +0100

    Delay.js added

---
+++ Delay.js
@@ -0,0 +1,37 @@
+function Delay(time,times,input,output){
+	this.delayNodes;
+	this.input = input;
+	this.times = times;
+	this.output = output;
+	this.lastdelay;
+	for (var i = times.length - 1; i >= 0; i--) {
+		var delay = acontext.createDelay();
+		delay.delayTime = time;
+		this.delayNodes.push(delay);
+
+		//initialize with first delay
+		if(i === 0) { 
+			lastdelay = delay;
+			this.input.connect(delay);
+		}
+		//following delays will be connected to the one before
+		else{
+			this.lastdelay.connect(delay);
+
+			//finally, when last delay is reached it is connected to actual output
+			if(i === this.times.length){
+				this.delay.connect(this.output);
+			}
+		}
+
+		//remember last delay for next connection
+		this.lastdelay = delay;
+	};
+
+	this.changeDelayTime = function(time){
+		for (var i = delayNodes.length - 1; i >= 0; i--) {
+			delayNodes[i].delayTime = time;
+		};
+	}
+
+}
--- Reverb.js
+++ Reverb.js
@@ -1,14 +1,14 @@
-//Right now only uses premixBus gain, leave object param empty to do so
-function Reverb(file,object){
+//Right now only uses premixBus gain, leave input param empty to do so
+function Reverb(file,input){
 
 	this.file = file;
-	this.object = object;
+	this.input = input;
 	this.convolver = acontext.createConvolver();
 
-	if (this.object === undefined){
-		this.object = premixBus;
+	if (this.input === undefined){
+		this.input = premixBus;
 	}
-	this.object.connect(this.convolver);
+	this.input.connect(this.convolver);
 	this.convolver.connect(fxBus);
 
 	this.loadIR = function(string){
