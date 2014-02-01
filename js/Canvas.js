b23dd81a6a309d0bd2567fc95d2e9831166580c3
Roman Arzaroli <roman.arzaroli@gmail.com>
2014-02-01 13:54:31 +0100

    Commented imgController and Canvas

--- js/Canvas.js
+++ js/Canvas.js
@@ -18,12 +18,15 @@ $(document).on("topchosen", topChosenHandler);
 
 $(document).on("botchosen", bottomChosenHandler);
 
-function zoomHandler(){
-    imgCon.scale = parsedPosition[2];
-}
-
-$(document).on("newposition", zoomHandler);
-
+//Das hier ist der DrawLoop. Er wird angestoßen durch
+//image.onload = drawLoop(); (weiter unten) und ruft sich
+//dann immer wieder selbst auf [siehe (requestAnimationFrame(drawLoop)]
+//nachdem der Browser uns nen neuen Frame gerendert hat.
+//Damit es hier aufgeraeumt bleibt habe ich eine Klasse imageController
+//erstellt, die die aktuelle Beschaffenheit des Bildes repräsentiert.
+//Der DrawLoop holt sich die informationen, wie das Bild im jetzigen Frame
+//zu rendern ist immer vom imgCon (siehe: var scale = imgCon.scale (...)
+//context.globalAlpha = imgCon.getAlpha()).
 function drawLoop() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	var w = canvas.width;
@@ -35,6 +38,31 @@ function drawLoop() {
     context.drawImage(image, -sw / 2 + w / 2, -sh / 2 + h / 2, sw, sh);
     requestAnimationFrame(drawLoop);
 }
+//Der imageController wird durch Events angesteuert und verändert,
+//was hier beim newPositionHandler() deutlich wird.
+//Er wird ausgeführt, wenn ein Event "newposition" gefeuert wurde
+// (das macht der Leap-Loop in der init.js, immer wenn ein neuer LeapFrame und somit
+// neue Positionen da sind, automatisch).
+// Immer dann gibts neue Werte im global verfügbaren parsedPosition-Array.
+// parsedPosition[0] ist X wert 0-1, parsedPosition[1] ist Y wert 0-1, parsedPosition[2] ist Z wert 0-1.
+
+//im newPositionHandler wird das imgController Objekt imgCon in seinen Attributen verändert.
+//Auf diese wird Dann in der obigen drawLoop() Funktion zurückgegriffen, um den aktuellen Zustand des Bildes
+//(wie ist es gezoomt? wo sind verschiedene Elemente? etc). zu untersuchen und dann entsprechende
+//Renderanweisungen an den Canvas zu geben. Momentan hab ich nur den Zoom eingebaut.
+
+//was du machen müsstest: den newPositionHandler() erweitern, um Werte im imgCon zu verändern, und
+//dann im drawloop ganz viele html5 canvas draw methoden schreiben, die diese werte entgegennehmen und daraus
+//das aktuelle Bild malen.
+function newPositionHandler(){
+    imgCon.scale = parsedPosition[2];
+    //Zum Beispiel hier: imgCon.gradientPosition = canvas.width*parsedPosition[0];
+    //um den imgCon zu aktualisieren. Die Anweisung zum Rendern müsste
+    //dann im drawLoop stehen, z.B. context.createRadialGradient(imgCon.gradientPosition...[sowie weitere Parameter,
+    // die die Funktion so braucht])
+}
+
+$(document).on("newposition", newPositionHandler());
 
 window.requestAnimationFrame = (function(){
   return  window.requestAnimationFrame       ||
--- js/ImageController.js
+++ js/ImageController.js
@@ -1,6 +1,13 @@
+//Dieses Objekt repräsentiert den aktuellen Zustand des Bildes
+//und wird im drawLoop abgefragt und von Events verändert.
 function ImageController(){
 
+    //Skalierung des Bildes
 	this.scale = 1;
+    //Position des radial gradients
+    this.gradientPosition = 0;
+    //hier ist Platz für mehr :D hinzufügen mit this.attributName = Wert;
+    // es ist dann abfragbar mit imgCon.attributName
 
 	var beatAnimRunning = false;
 
